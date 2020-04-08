f13fb8d10118ef0d9ebac4d7115224ed86664037
KrasimirKolchev <49152390+KrasimirKolchev@users.noreply.github.com>
2020-04-08 16:02:10 +0300

    Delete LabelService.java

--- SpringData/AdvancedQuerying/src/main/java/advancedquerying/service/labelSvc/LabelService.java
+++
@@ -1,9 +0,0 @@
-package advancedquerying.service.labelSvc;
-
-import advancedquerying.domain.entities.Label;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface LabelService {
-    Label getLabelById(long id);
-}
